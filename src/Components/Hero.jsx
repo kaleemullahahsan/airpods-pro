3b43e801e6f1936a011a4808bafda22aaf602285
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-06-14 21:18:58 +0500

    new changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,8 +1,7 @@
-import heroBg from "/images/tinywow_1_2_4254356 2.png";
 function Hero() {
   return (
     <section data-aos="fade-up" className=" h-screen mt-10 bg-size-[200px] md:bg-size-[400px] w-screen bg-center  bg-no-repeat flex justify-center  items-end"
-      style={{ backgroundImage: `url(${heroBg})` }}>
+      style={{ backgroundImage: `url(/images/tinywow_1_2_4254356 2.png)` }}>
       <div className="z-10 text-center flex justify-end items-center flex-col md:gap-20 gap-10" data-aos="fade-up">
         <h2 className="md:text-9xl text-6xl font-bold mt-0 ">AirPods Pro</h2>
         <div className="flex items-center gap-10 z-10">
