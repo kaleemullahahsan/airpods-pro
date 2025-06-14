4e6d89ff54f984d27b2fb064050b470a3fb9d6e5
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-06-14 21:07:16 +0500

    charging image fixed

--- src/Components/Charging.jsx
+++ src/Components/Charging.jsx
@@ -1,9 +1,8 @@
-import chargingBg from "/images/charging.png"
 function Charging() {
   return (
     <>
       <div className="text-black py-1 h-screen rounded-xl bg-cover bg-center"
-      style={{ backgroundImage: `url(${chargingBg})` }}>
+      style={{ backgroundImage: `url(/images/charging.png)` }}>
         <h3 className="md:text-6xl text-3xl font-bold text-center mt-4" data-aos="fade-in">
           Use tirelessly
         </h3>
