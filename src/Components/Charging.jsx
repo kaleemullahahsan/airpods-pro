e7bee581353c8b4800cff161ee8eba1a1ac76819
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-06-14 20:30:50 +0500

    Updated commit

--- src/Components/Charging.jsx
+++ src/Components/Charging.jsx
@@ -2,7 +2,7 @@ import chargingBg from "/images/charging.png"
 function Charging() {
   return (
     <>
-      <div className="bg-[url('images/charging.png')] text-black py-1 h-screen rounded-xl bg-cover bg-center" 
+      <div className="text-black py-1 h-screen rounded-xl bg-cover bg-center"
       style={{ backgroundImage: `url(${chargingBg})` }}>
         <h3 className="md:text-6xl text-3xl font-bold text-center mt-4" data-aos="fade-in">
           Use tirelessly
