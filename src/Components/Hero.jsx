c7f370ca71f340786af998e916857a062fbe44d7
Kaleemullah <kaleemullahahsan0@gmail.com>
2025-06-14 21:20:24 +0500

    debuggin

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
     <section data-aos="fade-up" className=" h-screen mt-10 bg-size-[200px] md:bg-size-[400px] w-screen bg-center  bg-no-repeat flex justify-center  items-end"
       style={{ backgroundImage: `url(/images/tinywow_1_2_4254356 2.png)` }}>
       <div className="z-10 text-center flex justify-end items-center flex-col md:gap-20 gap-10" data-aos="fade-up">
-        <h2 className="md:text-9xl text-6xl font-bold mt-0 ">AirPods Pro</h2>
+        <h2 className="md:text-9xl text-6xl font-bold mt-0 ">AirPods Prooo</h2>
         <div className="flex items-center gap-10 z-10">
           <a href="#">Learn more</a>
           <a
